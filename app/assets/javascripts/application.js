bcc9b60bbd781bec27ac67cfc9aeb59958860bff
MaksGostroushko <makshostroyshko@gmail.com>
2018-12-02 22:50:38 +0200

    removed trix editor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require rails-ujs
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require trix
 //= require_tree .
-//= require bootstrap-sprockets
