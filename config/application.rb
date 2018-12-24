369703abaf2c41f13a04c4e53cba458a5becd3ee
MaksGostroushko <makshostroyshko@gmail.com>
2018-12-24 10:10:38 +0200

    return to first version

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,8 @@ module SampleApp
     config.load_defaults 5.2
 
     # Include the authenticity token in remote forms.
-    config.assets.initialize_on_precompile = false #new
-    config.action_view.embed_authenticity_token_in_remote_forms = true
+    # config.assets.initialize_on_precompile = false #new
+    # config.action_view.embed_authenticity_token_in_remote_forms = true
     # config.secret_key_base = "421aa1287e015d358450fde71e6d325f6b9e34055b5c78f3e2c4b08c893cd910e0e8d1ffdbf5026b0699ec4e64e03ad9b6c795fe07f2ba644bfa0f9142c8bafd"
   end
 end
